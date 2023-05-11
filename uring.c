d91594b08f0af1cd1f377d435db36ac591a09dca
Arman Bilge <armanbilge@gmail.com>
2023-05-11 01:45:30 +0000

    Fixing

--- uring.c
+++ uring.c
@@ -25,9 +25,20 @@ int main(int argc, char *argv[])
   while (true) {
     struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
     struct io_uring_cqe *cqe;
-    io_uring_prep_send(sqe, fd, req, sizeof(req) - 1, MSG_NOSIGNAL);
-    io_uring_submit(&ring);
-    io_uring_wait_cqe(&ring, &cqe);
+    io_uring_prep_send(sqe, fd, req, strlen(req), MSG_NOSIGNAL);
+    
+    int ret = io_uring_submit(&ring);
+    if (ret != 1) {
+      perror("io_uring_submit");
+      return 1;
+    }
+    
+    ret = io_uring_wait_cqe(&ring, &cqe);
+    if (ret < 0) {
+      perror("io_uring_wait_cqe");
+      return 1;
+    }
+    
     io_uring_cqe_seen(&ring, cqe);
   }
 }
